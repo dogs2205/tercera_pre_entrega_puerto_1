341abbe47ed4402a071ad0318d29a3f14485a3e7
NICOLAS_PUERTO <puertonicolas98@gmail.com>
2023-03-11 02:10:09 -0500

    Agregado usuario

--- Granja/models.py
+++ Granja/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Post(models.Model):
+    nombre_animal = models.CharField(max_length=30)
+    tipo_animal = models.CharField(max_length=80)
+    edad_animal = models.CharField(max_length=15)
+    pre_diagnostico = models.CharField(max_length=120)
+
+    def __str__(self):
+        return f"{self.id} - {self.tipo_animal}"
+    
 # Create your models here.
